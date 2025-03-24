f349a01d84e79f990c18bb63477d4a1c9acde43b
nargiza <ravshankhasan@gmail.com>
2025-03-24 19:09:36 -0400

    create providers

---
+++ backend/terraform/providers.tf
@@ -0,0 +1,13 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"  # Укажи нужный регион AWS
+}
+
+# Configure the Terraform backend (optional)
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state"  
+    key    = "terraform/state"     
+    region = "us-east-1"           
+  }
+}
