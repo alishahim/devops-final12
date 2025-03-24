fbff0f62c8fb93dac46938dd5a011f36a08547e4
nargiza <ravshankhasan@gmail.com>
2025-03-24 19:19:06 -0400

    new prov tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"  # Укажи нужный регион AWS
 }
 
-# Configure the Terraform backend (optional)
+# Configure the Terraform backend (если используешь S3)
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state"  # 
-    key    = "terraform/state"     #
-    region = "us-east-1"           # 
+    bucket = "my-terraform-state"  # Имя твоего S3-бакета
+    key    = "terraform/state"     # Путь к файлу состояния
+    region = "us-east-1"           # Регион S3-бакета
   }
 }
